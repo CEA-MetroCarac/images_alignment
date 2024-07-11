7b9091e8006568ecd62fb3e635f79f3d10c0ace5
patquem <patrick.quemere@cea.fr>
2024-07-11 15:01:25 +0200

    simplify the examples

--- images_alignment/examples/example.py
+++ images_alignment/examples/example.py
@@ -11,7 +11,6 @@ from skimage.transform import rotate
 from skimage.io import imsave
 
 from images_alignment import ImagesAlign
-from images_alignment.application.app import App
 
 
 class UserTempDirectory:
@@ -37,74 +36,67 @@ def moving_image_generation(radius):
     return img
 
 
-def example(dirfunc, panel_app=False):
+def images_generation(dirname):
+    """ Generate the set of images to handle """
+
+    # fixed image (high resolution squared image)
+    img1 = shepp_logan_phantom()
+    fname_fixed = dirname / 'img1.tif'
+    imsave(fname_fixed, img1)
+    fnames_fixed = [fname_fixed]
+
+    # moving images (low resolution rectangular images)
+    img2 = moving_image_generation(radius=4)
+    imsave(dirname / 'img2.tif', img2)
+    fnames_moving = []
+    for k in range(3):
+        img2 = moving_image_generation(radius=2 + k)
+        fname_moving = dirname / f'img2_{k + 1}.tif'
+        imsave(fname_moving, img2)
+        fnames_moving.append(fname_moving)
+
+    return fnames_fixed, fnames_moving
+
+
+def example(dirname):
     """ Example based on 3 duplicated moving images with additional patterns """
+
+    fnames_fixed, fnames_moving = images_generation(dirname)
+
+    imgalign = ImagesAlign(fnames_fixed=fnames_fixed,
+                           fnames_moving=fnames_moving,
+                           thresholds=[0.15, 0.15],
+                           bin_inversions=[False, False],
+                           mode_auto=False)
+
+    plt.close()  # to close the default figure
+    fig0, ax0 = plt.subplots(1, 3, figsize=(12, 4))
+    fig0.suptitle("Original images")
+    imgalign.plot(ax=ax0, mode="Gray")
+
+    imgalign.cropping(1, area_percent=[0.40, 0.95, 0.25, 1.00])
+    imgalign.resizing()
+    imgalign.binarization()
+    imgalign.registration(registration_model='StackReg')
+
+    # apply the transformation to the set of images
+    imgalign.apply(dirname_res=dirname / 'results')
+
+    fig1, ax1 = plt.subplots(1, 3, figsize=(12, 4))
+    fig2, ax2 = plt.subplots(1, 3, figsize=(12, 4))
+    fig1.suptitle("Processed images (Gray mode)")
+    fig2.suptitle("Processed images (Binarized mode)")
+    imgalign.plot(ax=ax1, mode="Gray")
+    imgalign.plot(ax=ax2, mode="Binarized")
+    plt.show()
+
+
+if __name__ == '__main__':
+    # dirfunc = UserTempDirectory  # use the user temp location
+    dirfunc = tempfile.TemporaryDirectory  # use a TemporaryDirectory
 
     with dirfunc() as tmpdir:
         dirname = Path(tmpdir) / "images_alignement"
         dirname.mkdir(exist_ok=True)
 
-        # fixed image (high resolution squared image)
-        img1 = shepp_logan_phantom()
-        fname_fixed = dirname / 'img1.tif'
-        imsave(fname_fixed, img1)
-        fnames_fixed = [fname_fixed]
-
-        # moving images (low resolution rectangular images)
-        img2 = moving_image_generation(radius=4)
-        imsave(dirname / 'img2.tif', img2)
-        fnames_moving = []
-        for k in range(3):
-            img2 = moving_image_generation(radius=2 + k)
-            fname_moving = dirname / f'img2_{k + 1}.tif'
-            imsave(fname_moving, img2)
-            fnames_moving.append(fname_moving)
-
-        kwargs = {'fnames_fixed': fnames_fixed,
-                  'fnames_moving': fnames_moving,
-                  'thresholds': [0.15, 0.15],
-                  'bin_inversions': [False, False],
-                  'mode_auto': False}
-
-        if panel_app:
-            app = App(**kwargs)
-            imgalign = app.model
-
-        else:
-            imgalign = ImagesAlign(**kwargs)
-
-            plt.close()  # to close the default figure
-            fig0, ax0 = plt.subplots(1, 3, figsize=(12, 4))
-            fig0.suptitle("Original images")
-            imgalign.plot(ax=ax0, mode="Gray")
-
-        # cropping - resizing - binarization - registration
-        imgalign.cropping(1, area_percent=[0.40, 0.95, 0.25, 1.00])
-        imgalign.resizing()
-        imgalign.binarization()
-        imgalign.registration(registration_model='StackReg')
-
-        # apply the transformation to the set of images
-        imgalign.apply(dirname_res=dirname / 'results')
-
-        if panel_app:
-            app.window.show()
-
-        else:
-            fig1, ax1 = plt.subplots(1, 3, figsize=(12, 4))
-            fig2, ax2 = plt.subplots(1, 3, figsize=(12, 4))
-            fig1.suptitle("Processed images (Gray mode)")
-            fig2.suptitle("Processed images (Binarized mode)")
-            imgalign.plot(ax=ax1, mode="Gray")
-            imgalign.plot(ax=ax2, mode="Binarized")
-            plt.show()
-
-        return app
-
-
-if __name__ == '__main__':
-    my_dirfunc = UserTempDirectory  # use the user temp location
-    # my_dirfunc = tempfile.TemporaryDirectory  # use a TemporaryDirectory
-
-    # example(my_dirfunc)
-    example(my_dirfunc, panel_app=True)
+        example(dirname)
---
+++ images_alignment/examples/example_panel.py
@@ -0,0 +1,43 @@
+"""
+Example via the panel application
+"""
+from pathlib import Path
+import tempfile
+
+from images_alignment.application.app import App
+from example import images_generation, UserTempDirectory
+
+
+def example_panel(dirname):
+    """ Example based on 3 duplicated moving images with additional patterns """
+
+    fnames_fixed, fnames_moving = images_generation(dirname)
+
+    app = App(fnames_fixed=fnames_fixed,
+              fnames_moving=fnames_moving,
+              thresholds=[0.15, 0.15],
+              bin_inversions=[False, False],
+              mode_auto=False)
+
+    imgalign = app.model
+    imgalign.cropping(1, area_percent=[0.40, 0.95, 0.25, 1.00])
+    imgalign.resizing()
+    imgalign.binarization()
+    imgalign.registration(registration_model='StackReg')
+
+    # apply the transformation to the set of images
+    imgalign.apply(dirname_res=dirname / 'results')
+
+    app.update_plots()
+    app.window.show()
+
+
+if __name__ == '__main__':
+    # dirfunc = UserTempDirectory  # use the user temp location
+    dirfunc = tempfile.TemporaryDirectory  # use a TemporaryDirectory
+
+    with dirfunc() as tmpdir:
+        dirname = Path(tmpdir) / "images_alignement"
+        dirname.mkdir(exist_ok=True)
+
+        example_panel(dirname)
